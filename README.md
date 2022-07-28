a4a060cc4a60d30b98e60e372c833e4f6dc2dee5
Recillah Khamala <recillahk@gmail.com>
2022-07-28 21:41:21 +0300

    Add file changes

--- README.md
+++ README.md
@@ -2,61 +2,34 @@
 
 # Project Name
 
-> Description the project.
+> In this first capstone project, I have built an online website for a leadership conference..
 
 
 ## Built With
 
-- Major languages
-- Frameworks
-- Technologies used
+- HTML 
+- CSS
+- JavaScript
 
 ## Live Demo (if available)
 
 [Live Demo Link](https://livedemo.com)
 
 
-## Getting Started
-
-**This is an example of how you may give instructions on setting up your project locally.**
-**Modify this file to match your project, remove sections that don't apply. For example: delete the testing section if the currect project doesn't require testing.**
-
-
-To get a local copy up and running follow these simple example steps.
-
-### Prerequisites
-
-### Setup
-
-### Install
-
-### Usage
-
-### Run tests
-
-### Deployment
-
-
-
 ## Authors
 
 👤 **Author1**
 
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
+- GitHub: [@githubhandle](https://github.com/Rn486)
+- Twitter: [@twitterhandle](https://twitter.com/recillahk)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/recillah-khamala-071151b7/)
 
-👤 **Author2**
-
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
 
 ## 🤝 Contributing
 
 Contributions, issues, and feature requests are welcome!
 
-Feel free to check the [issues page](../../issues/).
+Feel free to check the [issues page](https://github.com/Rn486/HTML-CSS-JavaScript-capstone-project/issues/).
 
 ## Show your support
 
@@ -64,9 +37,7 @@ Give a ⭐️ if you like this project!
 
 ## Acknowledgments
 
-- Hat tip to anyone whose code was used
-- Inspiration
-- etc
+- Original design idea by Cindy Shin in Behance.
 
 ## 📝 License
 
--- js/programSpeakers.js
+++ js/programSpeakers.js
@@ -1,43 +1,43 @@
 const speakers = [
   {
     name: 'Recillah Neema Khamala',
-    profile: 'group Chief Customer Officer',
-    image: './images/professor1.svg',
+    profile: 'Group Chief Customer Officer',
+    image: '../images/professor1.svg',
     description:
       'President of consumer safety worldwide. Has over 10 yearf of experience leading customer experinec teams.',
   },
   {
     name: 'Meshack Wekesa Khamala',
     profile: 'Eden CEO',
-    image: './images/professor2.svg',
+    image: '../images/professor2.svg',
     description:
       'A true leader in his generation. Has successfuly started a media house from being a photographer. He hosts major events in the continent courtesy of his visionary skills.',
   },
   {
     name: 'Ressy Naomi Muricho',
     profile: 'Group CSO',
-    image: './images/professor3.svg',
+    image: '../images/professor3.svg',
     description:
       'Has successfuly lead sales teams in her 15 years of career. She is exceptionally good at closing deals and has seen expansion of her company to open banches beyound africa.',
   },
   {
     name: 'Paul Ngari Nyaga',
     profile: 'Yamagata Motors CEO',
-    image: './images/proffesor4.jpg',
+    image: '../images/proffesor4.jpg',
     description:
-      'A visionary leader in the motor industry. He has build his successful comapny from scratch and noe enjoys being the leading importer of vehicles in Africa.',
+      'A visionary leader in the motor industry. He has build his successful company from scratch and now enjoys being the leading importer of vehicles in Africa.',
   },
   {
     name: 'Annet Nyamvula williams',
     profile: 'Chief Marketing Officer',
-    image: './images/professor5.svg',
+    image: '../images/professor5.svg',
     description:
       'She has a wealth of experience in the  social-media marketing. He has singlehandedly led the brand awareness of her comapny and they are the leaders in the banking industry in Kenya',
   },
   {
     name: 'Faith Namwolo Muricho',
     profile: 'Global Chief Technology Officer',
-    image: './images/professor6.svg',
+    image: '../images/professor6.svg',
     description:
       'The 2020 winner of the most coveted EastAfrican HerNovation award. Global tech leader in the banking industry.',
   },
@@ -48,7 +48,7 @@ speakers.forEach((element) => {
   const speaker = document.createElement('div');
   speaker.innerHTML = `
     <div class="parent">
-      <img src="./images/chessBoard.jpeg" class="dev-bg-tiles" alt="chess" />
+      <img src="../images/chessBoard.jpeg" class="dev-bg-tiles" alt="chess" />
       <img src="${element.image}" alt="" class="developer-image" />
     </div>
     <div class="developer-info">
